31a5e0a491fdf4e3f35765e78d5c83fef585441c
manikanta <manikanta513@gmail.com>
2019-11-24 14:29:21 +0530

    --- Auto-Git Commit ---

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ status_check()
 {
 touch /tmp/output.txt
 >/tmp/output.txt
-/opt/lucidworks/fusion/3.1.3/bin/fusion status >> output.txt
+/opt/lucidworks/fusion/3.1.3/bin/fusion status > output.txt
 validate
 }
 
