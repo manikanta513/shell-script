e6496eebbc893e21b3b97395c34e58695d6c55da
manikanta <manikanta513@gmail.com>
2019-11-26 16:14:55 +0530

    --- Auto-Git Commit ---

--- RESTART_JVM.sh
+++ RESTART_JVM.sh
@@ -25,13 +25,13 @@ sc() {
 }
 
 pricing() {
-    ssh $2 "su -wasadm -c 'ps -ef | grep java | grep -v grep | xargs kill -9' "
+    ssh $2 "su -wasadm -c 'pkill -f java' "
     stat $? 
     ssh $2 "su -wasadm -c '/WCS/WebSphere70/AppServer/profiles/SCSharedA*/bin/startNode.sh; service jvma1_was.init start; service jvma2_was.init start'"
 }
 
 mylowes() {
-    ssh $2 "su - wasadm -c 'ps -ef | grep java | grep -v grep | xargs kill -9' "
+    ssh $2 "su - wasadm -c 'pkill -f java' "
     stat $? 
     ssh $2 "su - wasadm -c '/WCS/WebSphere70/AppServer/profiles/MyLowesA*/bin/startNode.sh; service jvma1_was.init start; service jvma2_was.init start'"
 }
