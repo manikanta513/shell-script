aa56a2b126fb548cd78ee5b7fc50f9bccb26c89b
manikanta <manikanta513@gmail.com>
2019-11-26 15:05:44 +0530

    --- Auto-Git Commit ---

--- RESTART_JVM.sh
+++ RESTART_JVM.sh
@@ -29,6 +29,12 @@ pricing(){
     ssh $2 "su -wasadm -c '/WCS/WebSphere70/AppServer/profiles/SCSharedA*/bin/startNode.sh; service jvma1_was.init start; service jvma2_was.init start'"
 }
 
+pricing(){
+    ssh $2 "su -wasadm -c 'ps -ef | grep java | grep -v grep | awk -F' ' '{print $2}' |xargs kill -9' "
+    stat $? 
+    ssh $2 "su -wasadm -c '/WCS/WebSphere70/AppServer/profiles/SCSharedA*/bin/startNode.sh; service jvma1_was.init start; service jvma2_was.init start'"
+}
+
 
 
 
