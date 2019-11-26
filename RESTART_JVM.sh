45bb00594e97240655b5e024b1b10d44a2190495
manikanta <manikanta513@gmail.com>
2019-11-26 14:59:42 +0530

    --- Auto-Git Commit ---

--- RESTART_JVM.sh
+++ RESTART_JVM.sh
@@ -13,8 +13,8 @@ fi
 
 commerce(){
     ssh $2 "su -wasadm -c 'ps -ef | grep java | grep -v grep | awk -F' ' '{print $2}' |xargs kill -9' "
-    stat $?startNo
-    ssh $2 "su -wasadm -c '/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA*/bin/startNo'"
+    stat $?startNode.sh; service 
+    ssh $2 "su -wasadm -c '/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA*/bin/startNode.sh; service '"
 }
 
 
