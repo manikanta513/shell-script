dcb274069d0e341f3467602b498eccf8d7e2dd64
manikanta <manikanta513@gmail.com>
2019-11-26 15:27:30 +0530

    --- Auto-Git Commit ---

--- RESTART_JVM.sh
+++ RESTART_JVM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-server=
+server=$2
 stat()
 {
 if [ "$1" -ne 0 ]; then
@@ -12,9 +12,9 @@ fi
 
 
 commerce() {
-    ssh $2 "su - wasadm -c 'ps -ef | grep java | grep -v grep |xargs kill -9'"
+    ssh $server "su - wasadm -c 'ps -ef | grep java | grep -v grep |xargs kill -9'"
     stat $? 
-    ssh $2 "su - wasadm -c '/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA*/bin/startNode.sh; service jvma1_was.init start; service jvma2_was.init start; service jvma3_was.init'"
+    ssh $server "su - wasadm -c '/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA*/bin/startNode.sh; service jvma1_was.init start; service jvma2_was.init start; service jvma3_was.init'"
 }
 
 sc() {
