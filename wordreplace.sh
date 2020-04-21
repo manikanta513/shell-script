f84008c574250ffa5bf1f3e416ad87d11e088583
manikanta <manikanta513@gmail.com>
2020-04-21 17:35:33 +0530

    --- Auto-Git Commit ---

--- wordreplace.sh
+++ wordreplace.sh
@@ -4,6 +4,6 @@ for i in `cat /usr/local/etc/group/cpi` ; do
         h=`echo -n $i | tail -c 2`
         echo $i
         echo $h
-        ssh $i "sed -i 's/i01/i$i/g /etc/init.d/application_task "
+        ssh $i "sed -i 's/i01/i$i/g' /etc/init.d/application_task"
         echo "++++++++++++++++++++Done for $i ++++++++++++++++++ "
 done
