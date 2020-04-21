b7da5098f02110e65c41de00371acd3171fdcea8
manikanta <manikanta513@gmail.com>
2020-04-21 17:34:03 +0530

    --- Auto-Git Commit ---

--- wordreplace.sh
+++ wordreplace.sh
@@ -4,6 +4,6 @@ for i in `cat /usr/local/etc/group/cpi` ; do
         h=`echo -n $i | tail -c 2`
         echo $i
         echo $h
-        ssh $i "/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA$h/bin/stopNode.sh"
+        ssh $i "sed -i /WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA$h/bin/stopNode.sh"
         echo "++++++++++++++++++++Done for $i ++++++++++++++++++ "
 done
