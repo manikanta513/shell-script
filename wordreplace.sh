fac4b32431eac271af4a05987bd661e42e26f00e
manikanta <manikanta513@gmail.com>
2020-04-21 17:15:01 +0530

    --- Auto-Git Commit ---

--- wordreplace.sh
+++ wordreplace.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+for i in `cat /usr/local/etc/group/commerce_restarts` ; do
+
+        h=`echo -n $i | tail -c 2`
+        echo $h
+        ssh $i "su - wasadm -c '/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA$h/bin/stopNode.sh'"
+        ssh $i "su - wasadm -c '/WCS/WebSphere70/AppServer/profiles/Lowes7OnlineA$h/bin/startNode.sh'"
+                echo "++++++++++++++++++++Done for $i ++++++++++++++++++ "
+done
